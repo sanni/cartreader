2a0ca1b236004dcf4914c377a5eaba12630902da
sanni <sanni@users.noreply.github.com>
2017-09-15 17:51:41 +0200

    V28D: Simplify GBA Logo Check

--- Cart_Reader/Cart_Reader.ino
+++ Cart_Reader/Cart_Reader.ino
@@ -2,8 +2,8 @@
                     Cartridge Reader for Arduino Mega2560
 
    Author:           sanni
-   Date:             2017-09-14
-   Version:          V28C
+   Date:             2017-09-15
+   Version:          V28D
 
    SD  lib:         https://github.com/greiman/SdFat
    LCD lib:         https://github.com/adafruit/Adafruit_SSD1306
@@ -34,7 +34,7 @@
    YamaArashi - GBA flashrom bank switch command
 
 **********************************************************************************/
-char ver[5] = "V28C";
+char ver[5] = "V28D";
 
 /******************************************
    Define Output
