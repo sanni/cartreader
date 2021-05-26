24495ab169e79f1f2fc239345fff955b0290e3bf
sanni <sanni@users.noreply.github.com>
2021-05-26 16:29:57 +0200

    V6.2: Add option to manually config N64 repro

--- Cart_Reader/Cart_Reader.ino
+++ Cart_Reader/Cart_Reader.ino
@@ -1,8 +1,8 @@
 /**********************************************************************************
                     Cartridge Reader for Arduino Mega2560
 
-   Date:             26.04.2021
-   Version:          6.1
+   Date:             26.05.2021
+   Version:          6.2
 
    SD lib: https://github.com/greiman/SdFat
    LCD lib: https://github.com/adafruit/Adafruit_SSD1306
@@ -43,7 +43,7 @@
 
 **********************************************************************************/
 
-char ver[5] = "6.1";
+char ver[5] = "6.2";
 
 /******************************************
    Libraries
