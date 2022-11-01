sanni/cartreader

commit a555f2117f54ca50fb57b46f1bda7732a8eed18f

All: Resolve all compiler and linker warnings
No functional change intended.

Cart_Reader/SFM.ino
@@ -941,7 +941,7 @@ void readROM_SFM() {
     display_Update();
 
     // Read up to 96 banks starting at bank 0×00.
-    for (int currBank = 0; currBank < numBanks; currBank++) {
+    for (byte currBank = 0; currBank < numBanks; currBank++) {
       // Dump the bytes to SD 512B at a time
       for (long currByte = 32768; currByte < 65536; currByte += 512) {
         for (int c = 0; c < 512; c++) {
@@ -956,7 +956,7 @@ void readROM_SFM() {
     println_Msg(F("Dumping HiRom..."));
     display_Update();
 
-    for (int currBank = 192; currBank < (numBanks + 192); currBank++) {
+    for (byte currBank = 192; currBank < (numBanks + 192); currBank++) {
       for (long currByte = 0; currByte < 65536; currByte += 512) {
         for (int c = 0; c < 512; c++) {
           sdBuffer[c] = readBank_SFM(currBank, currByte + c);
@@ -1056,7 +1056,7 @@ void writeFlash_SFM(int startBank, uint32_t pos) {
 
     if (romType) {
       // Write hirom
-      for (int currBank = startBank; currBank < startBank + numBanks; currBank++) {
+      for (byte currBank = startBank; currBank < startBank + numBanks; currBank++) {
         // Fill SDBuffer with 1 page at a time then write it repeat until all bytes are written
         for (unsigned long currByte = 0; currByte < 0x10000; currByte += 128) {
           myFile.read(sdBuffer, 128);
@@ -1081,7 +1081,7 @@ void writeFlash_SFM(int startBank, uint32_t pos) {
       }
     } else {
       // Write lorom
-      for (int currBank = 0; currBank < numBanks; currBank++) {
+      for (byte currBank = 0; currBank < numBanks; currBank++) {
         for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte += 128) {
           myFile.read(sdBuffer, 128);
           // Write command sequence
@@ -1184,7 +1184,7 @@ byte blankcheck_SFM(int startBank) {
 
   byte blank = 1;
   if (romType) {
-    for (int currBank = startBank; currBank < startBank + numBanks; currBank++) {
+    for (byte currBank = startBank; currBank < startBank + numBanks; currBank++) {
       for (unsigned long currByte = 0; currByte < 0x10000; currByte++) {
         if (readBank_SFM(currBank, currByte) != 0xFF) {
           currBank = startBank + numBanks;
@@ -1193,7 +1193,7 @@ byte blankcheck_SFM(int startBank) {
       }
     }
   } else {
-    for (int currBank = 0; currBank < numBanks; currBank++) {
+    for (byte currBank = 0; currBank < numBanks; currBank++) {
       for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte++) {
         if (readBank_SFM(currBank, currByte) != 0xFF) {
           currBank = numBanks;
@@ -1221,7 +1221,7 @@ unsigned long verifyFlash_SFM(int startBank, uint32_t pos) {
     controlIn_SFM();
 
     if (romType) {
-      for (int currBank = startBank; currBank < startBank + numBanks; currBank++) {
+      for (byte currBank = startBank; currBank < startBank + numBanks; currBank++) {
         for (unsigned long currByte = 0; currByte < 0x10000; currByte += 512) {
           // Fill SDBuffer
           myFile.read(sdBuffer, 512);
@@ -1233,7 +1233,7 @@ unsigned long verifyFlash_SFM(int startBank, uint32_t pos) {
         }
       }
     } else {
-      for (int currBank = 0; currBank < numBanks; currBank++) {
+      for (byte currBank = 0; currBank < numBanks; currBank++) {
         for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte += 512) {
           // Fill SDBuffer
           myFile.read(sdBuffer, 512);
@@ -1273,7 +1273,7 @@ void readFlash_SFM() {
     print_Error(create_file_STR, true);
   }
   if (romType) {
-    for (int currBank = 0xC0; currBank < 0xC0 + numBanks; currBank++) {
+    for (byte currBank = 0xC0; currBank < 0xC0 + numBanks; currBank++) {
       for (unsigned long currByte = 0; currByte < 0x10000; currByte += 512) {
         for (int c = 0; c < 512; c++) {
           sdBuffer[c] = readBank_SFM(currBank, currByte + c);
@@ -1282,7 +1282,7 @@ void readFlash_SFM() {
       }
     }
   } else {
-    for (int currBank = 0; currBank < numBanks; currBank++) {
+    for (byte currBank = 0; currBank < numBanks; currBank++) {
       for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte += 512) {
         for (int c = 0; c < 512; c++) {
           sdBuffer[c] = readBank_SFM(currBank, currByte + c);
@@ -1348,10 +1348,10 @@ void printMapping() {
   // Read the mapping out of the first chip
   char buffer[3];
 
-  for (int currByte = 0xFF00; currByte < 0xFF50; currByte += 10) {
+  for (unsigned int currByte = 0xFF00; currByte < 0xFF50; currByte += 10) {
     for (int c = 0; c < 10; c++) {
       itoa(readBank_SFM(0xC0, currByte + c), buffer, 16);
-      for (int i = 0; i < 2 - strlen(buffer); i++) {
+      for (size_t i = 0; i < 2 - strlen(buffer); i++) {
         print_Msg(F("0"));
       }
       // Now print the significant bits

Cart_Reader/SNES.ino
@@ -204,8 +204,6 @@ void snesMenu() {
           display_Clear();
           // Change working dir to root
           sd.chdir("/");
-          // get current time
-          unsigned long startTime = millis();
           // start reading from cart
           readROM_SNES();
           // Internal Checksum
@@ -604,7 +602,7 @@ void readLoRomBanks(unsigned int start, unsigned int total, FsFile* file) {
   uint32_t totalProgressBar = (uint32_t)(total - start) * 1024;
   draw_progressbar(0, totalProgressBar);
 
-  for (int currBank = start; currBank < total; currBank++) {
+  for (byte currBank = start; currBank < total; currBank++) {
     PORTL = currBank;
 
     // Blink led
@@ -655,7 +653,7 @@ void readHiRomBanks(unsigned int start, unsigned int total, FsFile* file) {
   uint32_t totalProgressBar = (uint32_t)(total - start) * 1024;
   draw_progressbar(0, totalProgressBar);
 
-  for (int currBank = start; currBank < total; currBank++) {
+  for (byte currBank = start; currBank < total; currBank++) {
     PORTL = currBank;
 
     // Blink led
@@ -700,31 +698,6 @@ void readHiRomBanks(unsigned int start, unsigned int total, FsFile* file) {
 ******************************************/
 void getCartInfo_SNES() {
   boolean manualConfig = 0;
-
-  //Prime SA1 cartridge
-  uint16_t c = 0;
-  uint16_t currByte = 0;
-  byte buffer[1024] = { 0 };
-  PORTL = 192;
-  while (c < 1024) {
-    PORTF = (currByte & 0xFF);
-    PORTK = ((currByte >> 8) & 0xFF);
-
-    // Wait for the Byte to appear on the data bus
-    // Arduino running at 16Mhz -> one nop = 62.5ns
-    // slowRom is good for 200ns, fastRom is <= 120ns; S-CPU best case read speed: 3.57MHz / 280ns
-    // let's be conservative and use 6 x 62.5 = 375ns
-    NOP;
-    NOP;
-    NOP;
-    NOP;
-    NOP;
-    NOP;
-
-    buffer[c] = PINC;
-    c++;
-    currByte++;
-  }
 
   // Print start page
   if (checkcart_SNES() == 0) {
@@ -953,12 +926,10 @@ boolean checkcart_SNES() {
   // set control to read
   dataIn();
 
-  uint16_t c = 0;
   uint16_t headerStart = 0xFFB0;
-  uint16_t currByte = headerStart;
-  byte snesHeader[80] = { 0 };
+  byte snesHeader[80];
   PORTL = 0;
-  while (c < 80) {
+  for (uint16_t c = 0, currByte = headerStart; c < 80; c++, currByte++) {
     PORTF = (currByte & 0xFF);
     PORTK = ((currByte >> 8) & 0xFF);
 
@@ -976,8 +947,6 @@ boolean checkcart_SNES() {
     NOP;
 
     snesHeader[c] = PINC;
-    c++;
-    currByte++;
   }
 
   // Calculate CRC32 of header
@@ -1396,7 +1365,7 @@ void readROM_SNES() {
     controlIn_SNES();
     byte initialSOMap = readBank_SNES(0, 18439);
 
-    for (int currMemmap = 0; currMemmap < (numBanks / 16); currMemmap++) {
+    for (byte currMemmap = 0; currMemmap < (numBanks / 16); currMemmap++) {
 
       dataOut();
       controlOut_SNES();
@@ -1974,6 +1943,7 @@ unsigned long verifySRAM() {
     return writeErrors;
   } else {
     print_Error(F("Can't open file"), false);
+    return 1;
   }
 }
 

Cart_Reader/Cart_Reader.ino
@@ -564,6 +564,7 @@ uint32_t calculateCRC(char* fileName, char* folder, int offset) {
     //print_Msg(F("/"));
     //print_Msg(fileName);
     print_Error(F(" not found"), true);
+    return 0;
   }
 }
 
@@ -616,7 +617,7 @@ void get_line(char* str_buf, FsFile* readfile, uint8_t maxi) {
 }
 
 // Calculate CRC32 if needed and compare it to CRC read from database
-boolean compareCRC(char* database, char* crcString, boolean renamerom, int offset) {
+boolean compareCRC(const char* database, char* crcString, boolean renamerom, int offset) {
 #ifdef nointro
   char crcStr[9];
   if (crcString == 0) {
@@ -661,15 +662,15 @@ boolean compareCRC(char* database, char* crcString, boolean renamerom, int offse
           get_line(iNES_STR, &myFile, 33);
 
           // Convert "4E4553" to (0x4E, 0x45, 0x53)
-          byte iNES_BUF[2];
+          unsigned int iNES_BUF;
           for (byte j = 0; j < 16; j++) {
-            sscanf(iNES_STR + j * 2, "%2X", iNES_BUF);
-            iNES_HEADER[j] = iNES_BUF[0];
+            sscanf(iNES_STR + j * 2, "%2X", &iNES_BUF);
+            iNES_HEADER[j] = iNES_BUF;
           }
           //Skip CRLF
           myFile.seekSet(myFile.curPosition() + 4);
         }
-#endif
+#endif // enable_NES
 
         // Close the file:
         myFile.close();
@@ -687,7 +688,7 @@ boolean compareCRC(char* database, char* crcString, boolean renamerom, int offse
           }
           myFile.close();
         }
-#endif
+#endif // enable_NES
         print_Msg(F(" -> "));
         display_Update();
 
@@ -718,9 +719,10 @@ boolean compareCRC(char* database, char* crcString, boolean renamerom, int offse
     println_Msg(F("Database missing"));
     return 0;
   }
-#else
+#else // nointro
   println_Msg("");
-#endif
+#endif // !nointro
+  return 0;
 }
 
 byte starting_letter() {
@@ -1877,7 +1879,7 @@ void print_Error(const __FlashStringHelper* errorMessage, boolean forceReset) {
 
   if (forceReset) {
     println_Msg(F(""));
-    println_Msg(F("Press Button..."));
+    print_STR(press_button_STR, 1);
     display_Update();
     wait();
     if (ignoreError == 0) {
@@ -1997,7 +1999,7 @@ void save_log() {
       }
       myFile.write(sdBuffer, 512);
     } else {
-      word i = 0;
+      int i = 0;
       for (; i < myLog.available(); i++) {
         sdBuffer[i] = myLog.read();
       }
@@ -2031,7 +2033,7 @@ void print_Msg(const char myString[]) {
 #if (defined(enable_LCD) || defined(enable_OLED))
   // test for word wrap
   if ((display.tx + strlen(myString) * 6) > 128) {
-    int strPos = 0;
+    unsigned int strPos = 0;
     // Print until end of display
     while (display.tx < 122) {
       display.print(myString[strPos]);
@@ -2174,7 +2176,7 @@ void println_Msg(const char myString[]) {
 #if (defined(enable_LCD) || defined(enable_OLED))
   // test for word wrap
   if ((display.tx + strlen(myString) * 6) > 128) {
-    int strPos = 0;
+    unsigned int strPos = 0;
     // Print until end of display
     while ((display.tx < 122) && (myString[strPos] != '\0')) {
       display.print(myString[strPos]);
@@ -2305,8 +2307,8 @@ void blinkLED() {
 #endif
 }
 
-void statusLED(boolean on) {
 #if defined(HW5)
+void statusLED(boolean on) {
   if (!on)
     PORTD |= (1 << 7);
   else
@@ -2334,8 +2336,11 @@ void statusLED(boolean on) {
       PORTB &= ~(1 << 7);
     }
   */
-#endif
 }
+#else
+void statusLED(boolean on __attribute__ ((unused))) {
+}
+#endif
 
 /******************************************
   Menu system

Cart_Reader/FLASH.ino
@@ -1803,7 +1803,7 @@ void printFlash(int numBytes) {
   for (int currByte = 0; currByte < numBytes; currByte += 10) {
     for (int c = 0; c < 10; c++) {
       itoa(readByte_Flash(currByte + c), myBuffer, 16);
-      for (int i = 0; i < 2 - strlen(myBuffer); i++) {
+      for (size_t i = 0; i < 2 - strlen(myBuffer); i++) {
         print_Msg(F("0"));
       }
       // Now print the significant bits
@@ -2147,14 +2147,14 @@ void printFlash16(int numBytes) {
 
 
       sprintf(buf, "%x", left_byte);
-      for (int i = 0; i < 2 - strlen(buf); i++) {
+      for (size_t i = 0; i < 2 - strlen(buf); i++) {
         print_Msg(F("0"));
       }
       // Now print the significant bits
       print_Msg(buf);
 
       sprintf(buf, "%x", right_byte);
-      for (int i = 0; i < 2 - strlen(buf); i++) {
+      for (size_t i = 0; i < 2 - strlen(buf); i++) {
         print_Msg(F("0"));
       }
       // Now print the significant bits
@@ -2546,14 +2546,14 @@ void print_Eprom(int numBytes) {
 
 
       sprintf(buf, "%x", left_byte);
-      for (int i = 0; i < 2 - strlen(buf); i++) {
+      for (size_t i = 0; i < 2 - strlen(buf); i++) {
         print_Msg(F("0"));
       }
       // Now print the significant bits
       print_Msg(buf);
 
       sprintf(buf, "%x", right_byte);
-      for (int i = 0; i < 2 - strlen(buf); i++) {
+      for (size_t i = 0; i < 2 - strlen(buf); i++) {
         print_Msg(F("0"));
       }
       // Now print the significant bits

Cart_Reader/GB.ino
@@ -7,8 +7,8 @@
    Variables
  *****************************************/
 // Game Boy
-int sramBanks;
-int romBanks;
+word sramBanks;
+word romBanks;
 word lastByte = 0;
 
 /******************************************
@@ -911,19 +911,41 @@ void getCartInfo_GB() {
   }
 
   // M161 (Mani 4 in 1)
-  if ((strncmp(romName, "TETRIS SET", 10) == 0) && (sdBuffer[0x14D] == 0x3F)) {
+  if (strncmp(romName, "TETRIS SET", 10) == 0 && sdBuffer[0x14D] == 0x3F) {
     romType = 0x104;
   }
 
   // MMM01 (Mani 4 in 1)
   if (
-    (strncmp(romName, "BOUKENJIMA2 SET", 15) == 0) && (sdBuffer[0x14D] == 0) || (strncmp(romName, "BUBBLEBOBBLE SET", 16) == 0) && (sdBuffer[0x14D] == 0xC6) || (strncmp(romName, "GANBARUGA SET", 13) == 0) && (sdBuffer[0x14D] == 0x90) || (strncmp(romName, "RTYPE 2 SET", 11) == 0) && (sdBuffer[0x14D] == 0x32)) {
+    (
+      strncmp(romName, "BOUKENJIMA2 SET", 15) == 0 && sdBuffer[0x14D] == 0
+    ) || (
+      strncmp(romName, "BUBBLEBOBBLE SET", 16) == 0 && sdBuffer[0x14D] == 0xC6
+    ) || (
+      strncmp(romName, "GANBARUGA SET", 13) == 0 && sdBuffer[0x14D] == 0x90
+    ) || (
+      strncmp(romName, "RTYPE 2 SET", 11) == 0 && sdBuffer[0x14D] == 0x32
+    )
+  ) {
     romType = 0x0B;
   }
 
   // MBC1M
   if (
-    (strncmp(romName, "MOMOCOL", 7) == 0) && (sdBuffer[0x14D] == 0x28) || (strncmp(romName, "BOMCOL", 6) == 0) && (sdBuffer[0x14D] == 0x86) || (strncmp(romName, "GENCOL", 6) == 0) && (sdBuffer[0x14D] == 0x8A) || (strncmp(romName, "SUPERCHINESE 123", 16) == 0) && (sdBuffer[0x14D] == 0xE4) || (strncmp(romName, "MORTALKOMBATI&II", 16) == 0) && (sdBuffer[0x14D] == 0xB9) || (strncmp(romName, "MORTALKOMBAT DUO", 16) == 0) && (sdBuffer[0x14D] == 0xA7)) {
+    (
+      strncmp(romName, "MOMOCOL", 7) == 0 && sdBuffer[0x14D] == 0x28
+    ) || (
+      strncmp(romName, "BOMCOL", 6) == 0 && sdBuffer[0x14D] == 0x86
+    ) || (
+      strncmp(romName, "GENCOL", 6) == 0 && sdBuffer[0x14D] == 0x8A
+    ) || (
+      strncmp(romName, "SUPERCHINESE 123", 16) == 0 && sdBuffer[0x14D] == 0xE4
+    ) || (
+      strncmp(romName, "MORTALKOMBATI&II", 16) == 0 && sdBuffer[0x14D] == 0xB9
+    ) || (
+      strncmp(romName, "MORTALKOMBAT DUO", 16) == 0 && sdBuffer[0x14D] == 0xA7
+    )
+  ) {
     romType += 0x100;
   }
 
@@ -961,7 +983,7 @@ void readROM_GB() {
     print_Error(create_file_STR, true);
   }
 
-  int endAddress = 0x7FFF;
+  word endAddress = 0x7FFF;
   word romAddress = 0;
   word startBank = 1;
 
@@ -1082,7 +1104,7 @@ void readROM_GB() {
 }
 
 // Calculate checksum
-unsigned int calc_checksum_GB(char* fileName, char* folder) {
+unsigned int calc_checksum_GB(char* fileName) {
   unsigned int calcChecksum = 0;
   //  int calcFilesize = 0; // unused
   unsigned long i = 0;
@@ -1126,7 +1148,7 @@ void compare_checksums_GB() {
 
   // Internal ROM checksum
   char calcsumStr[5];
-  sprintf(calcsumStr, "%04X", calc_checksum_GB(fileName, folder));
+  sprintf(calcsumStr, "%04X", calc_checksum_GB(fileName));
 
   print_Msg(F("Checksum: "));
   print_Msg(calcsumStr);
@@ -1299,6 +1321,7 @@ unsigned long verifySRAM_GB() {
     return writeErrors;
   } else {
     print_Error(open_file_STR, true);
+    return 1;
   }
 }
 
@@ -1679,7 +1702,7 @@ void writeFlash29F_GB(byte MBC, boolean flashErase) {
       display_Update();
 
       // Read x number of banks
-      for (int currBank = 0; currBank < romBanks; currBank++) {
+      for (word currBank = 0; currBank < romBanks; currBank++) {
         // Blink led
         blinkLED();
 
@@ -1713,7 +1736,7 @@ void writeFlash29F_GB(byte MBC, boolean flashErase) {
       uint32_t totalProgressBar = (uint32_t)(romBanks)*16384;
       draw_progressbar(0, totalProgressBar);
 
-      for (int currBank = 0; currBank < romBanks; currBank++) {
+      for (word currBank = 0; currBank < romBanks; currBank++) {
         // Blink led
         blinkLED();
 
@@ -1763,7 +1786,7 @@ void writeFlash29F_GB(byte MBC, boolean flashErase) {
       uint32_t totalProgressBar = (uint32_t)(romBanks)*16384;
       draw_progressbar(0, totalProgressBar);
 
-      for (int currBank = 0; currBank < romBanks; currBank++) {
+      for (word currBank = 0; currBank < romBanks; currBank++) {
         // Blink led
         blinkLED();
 
@@ -1885,7 +1908,7 @@ byte readByteCompensated(int address) {
    .
    address needs to be the x8 mode address of the flash register that should be read.
 */
-byte writeByteCompensated(int address, byte data) {
+void writeByteCompensated(int address, byte data) {
   if (flashSwitchLastBits) {
     data = (data & 0b11111100) | ((data << 1) & 0b10) | ((data >> 1) & 0b01);
   }
@@ -2078,7 +2101,7 @@ bool writeCFI_GB() {
     display_Update();
 
     // Read x number of banks
-    for (int currBank = 0; currBank < romBanks; currBank++) {
+    for (word currBank = 0; currBank < romBanks; currBank++) {
       // Blink led
       blinkLED();
 
@@ -2105,7 +2128,7 @@ bool writeCFI_GB() {
     word currAddr = 0;
     word endAddr = 0x3FFF;
 
-    for (int currBank = 0; currBank < romBanks; currBank++) {
+    for (word currBank = 0; currBank < romBanks; currBank++) {
       // Blink led
       blinkLED();
 

Cart_Reader/GBA.ino
@@ -1339,6 +1339,7 @@ unsigned long verifySRAM_GBA(unsigned long sramSize, uint32_t pos) {
     return writeErrors;
   } else {
     print_Error(F("Can't open file"), false);
+    return 1;
   }
 }
 
@@ -1546,6 +1547,7 @@ unsigned long verifyFRAM_GBA(unsigned long framSize) {
     return writeErrors;
   } else {
     print_Error(F("Can't open file"), false);
+    return 1;
   }
 }
 

Cart_Reader/GBM.ino
@@ -308,7 +308,7 @@ void writeByte_GBM(word myAddress, byte myData) {
   HELPER FUNCTIONS
 **********************/
 void printSdBuffer(word startByte, word numBytes) {
-  for (int currByte = 0; currByte < numBytes; currByte += 10) {
+  for (word currByte = 0; currByte < numBytes; currByte += 10) {
     for (byte c = 0; c < 10; c++) {
       // Convert to char array so we don't lose leading zeros
       char currByteStr[2];

Cart_Reader/GBS.ino
@@ -62,7 +62,7 @@ boolean compare_checksum_GBS() {
   sprintf(folder, "GB/ROM/%s/%d", romName, foldern - 1);
 
   char calcsumStr[5];
-  sprintf(calcsumStr, "%04X", calc_checksum_GB(fileName, folder));
+  sprintf(calcsumStr, "%04X", calc_checksum_GB(fileName));
 
   if (strcmp(calcsumStr, checksumStr) == 0) {
     print_Msg(F("Result: "));
@@ -404,7 +404,7 @@ void gbSmartReadFlash() {
     print_Error(create_file_STR, true);
 
   // reset flash to read array state
-  for (int i = 0x00; i < gbSmartBanks; i += gbSmartBanksPerFlashChip)
+  for (uint16_t i = 0x00; i < gbSmartBanks; i += gbSmartBanksPerFlashChip)
     gbSmartResetFlash(i);
 
   // remaps mmc to full access
@@ -443,7 +443,7 @@ void gbSmartReadFlash() {
 }
 
 void gbSmartWriteFlash() {
-  for (int bank = 0x00; bank < gbSmartBanks; bank += gbSmartBanksPerFlashChip) {
+  for (uint16_t bank = 0x00; bank < gbSmartBanks; bank += gbSmartBanksPerFlashChip) {
     display_Clear();
 
     print_Msg(F("Erasing..."));

commit c82815f0d5d0e4f87e5fd4835ababe1912409a43

SFM.ino, SNES.ino: Rework compiler warnings resolution
I am not sure why I changed some variables from 16 bit types to 8 bit types
when the warnings being resolved were only about signedness.
Fixes: a555f21 ("All: Resolve all compiler and linker warnings")

Cart_Reader/SFM.ino
@@ -912,7 +912,7 @@ void readROM_SFM() {
     display_Update();
 
     // Read up to 96 banks starting at bank 0×00.
-    for (byte currBank = 0; currBank < numBanks; currBank++) {
+    for (word currBank = 0; currBank < numBanks; currBank++) {
       // Dump the bytes to SD 512B at a time
       for (long currByte = 32768; currByte < 65536; currByte += 512) {
         for (int c = 0; c < 512; c++) {
@@ -1064,7 +1064,7 @@ void writeFlash_SFM(int startBank, uint32_t pos) {
       draw_progressbar(0, totalProgressBar);
 
       // Write lorom
-      for (byte currBank = 0; currBank < numBanks; currBank++) {
+      for (word currBank = 0; currBank < numBanks; currBank++) {
         for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte += 128) {
           myFile.read(sdBuffer, 128);
           // Write command sequence
@@ -1179,7 +1179,7 @@ byte blankcheck_SFM(int startBank) {
       }
     }
   } else {
-    for (byte currBank = 0; currBank < numBanks; currBank++) {
+    for (word currBank = 0; currBank < numBanks; currBank++) {
       for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte++) {
         if (readBank_SFM(currBank, currByte) != 0xFF) {
           currBank = numBanks;
@@ -1219,7 +1219,7 @@ unsigned long verifyFlash_SFM(int startBank, uint32_t pos) {
         }
       }
     } else {
-      for (byte currBank = 0; currBank < numBanks; currBank++) {
+      for (word currBank = 0; currBank < numBanks; currBank++) {
         for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte += 512) {
           // Fill SDBuffer
           myFile.read(sdBuffer, 512);
@@ -1268,7 +1268,7 @@ void readFlash_SFM() {
       }
     }
   } else {
-    for (byte currBank = 0; currBank < numBanks; currBank++) {
+    for (word currBank = 0; currBank < numBanks; currBank++) {
       for (unsigned long currByte = 0x8000; currByte < 0x10000; currByte += 512) {
         for (int c = 0; c < 512; c++) {
           sdBuffer[c] = readBank_SFM(currBank, currByte + c);

Cart_Reader/SNES.ino
@@ -1344,7 +1344,7 @@ void readROM_SNES() {
     controlIn_SNES();
     byte initialSOMap = readBank_SNES(0, 18439);
 
-    for (byte currMemmap = 0; currMemmap < (numBanks / 16); currMemmap++) {
+    for (word currMemmap = 0; currMemmap < (numBanks / 16); currMemmap++) {
 
       dataOut();
       controlOut_SNES();