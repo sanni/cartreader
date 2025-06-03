77e290d77ae79a5ddc2c038860fa0070baa7a384
sanni <sanni@users.noreply.github.com>
2025-06-03 12:07:01 +0200

    Update launch_oscr_tool.bat

--- tools/oscr_tool/launch_oscr_tool.bat
+++ tools/oscr_tool/launch_oscr_tool.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -File oscr_tool.ps1
+powershell.exe -ExecutionPolicy RemoteSigned -File oscr_tool.ps1
 if errorlevel 1 (
     echo.
     echo [ERROR] Script failed. Press any key to continue...
